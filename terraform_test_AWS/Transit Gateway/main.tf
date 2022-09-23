137d0207c5bfd89ca183dee1f4db6387c1dd257c
bortolo <andrea.bortolossi89@gmail.com>
2022-09-23 18:15:03 +0200

    transit gateway portare chiave pubblica su jumphost per fare ssh su vm-a private

--- terraform_test_AWS/Transit Gateway/main.tf
+++ terraform_test_AWS/Transit Gateway/main.tf
@@ -167,6 +167,7 @@ resource "aws_instance" "jumphost" {
   instance_type               = "t3.micro"
   key_name                    = var.key_pair_name
   associate_public_ip_address = true
+  //user_data                   = data.cloudinit_config.example.rendered
   subnet_id                   = aws_subnet.a-public.id
   security_groups             = [aws_security_group.jumphost.id]
   tags = {
@@ -240,88 +241,104 @@ resource "aws_security_group_rule" "icmp-vm-a" {
   security_group_id = aws_security_group.vm-a.id
 }
 
-
-
 # VM private B
 
-# VM private C
+resource "aws_instance" "vm-b" {
+  ami                         = "ami-0bd39c806c2335b95"
+  instance_type               = "t3.micro"
+  key_name                    = var.key_pair_name
+  subnet_id                   = aws_subnet.b-private.id
+  security_groups             = [aws_security_group.vm-b.id]
+  tags = {
+    Name = "vm-b"
+  }
+}
 
+resource "aws_security_group" "vm-b" {
+  vpc_id      = aws_vpc.vpc-b.id
+  tags = {
+    Name = "vm-b"
+  }
+}
+
+resource "aws_security_group_rule" "ssh-vm-b" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+  security_group_id = aws_security_group.vm-b.id
+}
+
+resource "aws_security_group_rule" "icmp-vm-b" {
+  type              = "ingress"
+  from_port         = 8
+  to_port           = 0
+  protocol          = "icmp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.vm-b.id
+}
+
+# VM private C
+resource "aws_instance" "vm-c" {
+  ami                         = "ami-0bd39c806c2335b95"
+  instance_type               = "t3.micro"
+  key_name                    = var.key_pair_name
+  subnet_id                   = aws_subnet.c-private.id
+  security_groups             = [aws_security_group.vm-c.id]
+  tags = {
+    Name = "vm-c"
+  }
+}
+
+resource "aws_security_group" "vm-c" {
+  vpc_id      = aws_vpc.vpc-c.id
+  tags = {
+    Name = "vm-c"
+  }
+}
+
+resource "aws_security_group_rule" "ssh-vm-c" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+  security_group_id = aws_security_group.vm-c.id
+}
+
+resource "aws_security_group_rule" "icmp-vm-c" {
+  type              = "ingress"
+  from_port         = 8
+  to_port           = 0
+  protocol          = "icmp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.vm-c.id
+}
 
 /*
-data "aws_vpc" "default" {
-  default = true
-}
+data "cloudinit_config" "example" {
+  gzip          = false
+  base64_encode = false
 
-data "aws_subnet_ids" "all" {
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_key_pair" "this" {
-  key_name   = var.key_pair_name
-  public_key = var.public_key
-  tags = local.user_tag
-}
-
-module "ec2" {
-  //source                      = "github.com/terraform-aws-modules/terraform-aws-ec2-instance"
-  source                      = "../../modules_AWS/terraform-aws-ec2-instance-master"
-  name                        = var.ec2_name
-
-  instance_count              = 1
-  
-  ami                         = "ami-0bd39c806c2335b95" //AWS Linux AMI
-  instance_type               = "t2.micro"
-  key_name                    = var.key_pair_name
-  associate_public_ip_address = true
-  monitoring                  = false
-  vpc_security_group_ids      = [module.aws_security_group.security_group_id]
-  subnet_id                   = tolist(data.aws_subnet_ids.all.ids)[0]
-  user_data                   = data.cloudinit_config.example.rendered
-  iam_instance_profile        = module.iam_assumable_role_custom.iam_instance_profile_name
-
-  tags = local.user_tag
-}
-
-module "aws_security_group" {
-  source      = "github.com/terraform-aws-modules/terraform-aws-security-group"
-  name        = "security_group"
-  description = "Security group for Kinesis"
-  vpc_id      = data.aws_vpc.default.id
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      description = "SSH port"
-      cidr_blocks = "0.0.0.0/0"
-    },
-  ]
-  egress_with_cidr_blocks = [
-    {
-      from_port   = 0
-      to_port     = 0
-      protocol    = -1
-      description = "allow all outbound"
-      cidr_blocks = "0.0.0.0/0"
-    },
-  ]
-  tags = local.user_tag
-}
-
-module "iam_assumable_role_custom" {
-  source            = "github.com/terraform-aws-modules/terraform-aws-iam/modules/iam-assumable-role"
-  trusted_role_arns = []
-  trusted_role_services = [
-    "ec2.amazonaws.com"
-  ]
-  create_role             = true
-  create_instance_profile = true
-  role_name               = "ec2-admin-firehose"
-  role_requires_mfa       = false
-  custom_role_policy_arns = [
-    "arn:aws:iam::aws:policy/AmazonKinesisFullAccess",
-  ]
-
-  tags = local.user_tag
-}
-*/
+  part {
+    content_type = "text/cloud-config"
+    filename     = "cloud-config.yaml"
+    content      = <<-END
+      #cloud-config
+      ${jsonencode({
+        write_files = [
+                    {
+            path        = "/home/ec2-user/.ssh/id_rsa.pub"
+            permissions = "0644"
+            owner       = "root:root"
+            encoding    = "b64"
+            content     = filebase64("./resources/id_rsa.pub")
+          },
+        ]
+      })}
+    END
+  }
+}*/
