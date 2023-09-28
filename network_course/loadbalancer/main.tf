e57d3a898b5dafc30288536bdba81889cc8b523a
andrea <andrea.bortolossi89@gmail.com>
2023-09-28 16:56:17 +0200

    loadbalacing working in 2 AZs

--- network_course/loadbalancer/main.tf
+++ network_course/loadbalancer/main.tf
@@ -29,7 +29,7 @@ module "vpc" {
   }
 }
 
-module "security_group" {
+module "security_group_web" {
   source = "terraform-aws-modules/security-group/aws"
 
   name        = "security-group-web-server"
@@ -47,6 +47,53 @@ resource "aws_key_pair" "this" {
   public_key = file("./resources/id_rsa.pub")
 }
 
+module "bastion" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  name                        = "bastion"
+  ami                         = "ami-0a261c0e5f51090b1"
+  instance_type               = "t2.micro"
+  availability_zone           = element(module.vpc.azs, 0)
+  subnet_id                   = element(module.vpc.public_subnets, 0)
+  vpc_security_group_ids      = [module.security_group_web.security_group_id]
+  associate_public_ip_address = true
+  key_name                    = "ec2-key-pair"
+
+  create_iam_instance_profile = true
+  iam_role_description        = "IAM role for EC2 instance"
+  iam_role_policies = {
+    AdministratorAccess = "arn:aws:iam::aws:policy/AdministratorAccess"
+  }
+
+
+  enable_volume_tags = false
+  root_block_device = [
+    {
+      encrypted   = true
+      volume_type = "gp3"
+      throughput  = 200
+      volume_size = 50
+      tags = {
+        Name = "my-root-block"
+      }
+    },
+  ]
+
+  ebs_block_device = [
+    {
+      device_name = "/dev/sdf"
+      volume_type = "gp3"
+      volume_size = 5
+      throughput  = 200
+      encrypted   = false
+    }
+  ]
+
+    tags = local.tags
+
+}
+
+
 module "ec2" {
   source = "terraform-aws-modules/ec2-instance/aws"
 
@@ -55,7 +102,7 @@ module "ec2" {
   instance_type               = "t2.micro"
   availability_zone           = element(module.vpc.azs, 0)
   subnet_id                   = element(module.vpc.public_subnets, 0)
-  vpc_security_group_ids      = [module.security_group.security_group_id]
+  vpc_security_group_ids      = [module.security_group_web.security_group_id]
   associate_public_ip_address = true
   key_name                    = "ec2-key-pair"
 
@@ -93,4 +140,112 @@ module "ec2" {
 
     tags = local.tags
 
+}
+
+module "ec2-web2" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  name                        = "ec2-web2"
+  ami                         = "ami-0a261c0e5f51090b1"
+  instance_type               = "t2.micro"
+  availability_zone           = element(module.vpc.azs, 1)
+  subnet_id                   = element(module.vpc.public_subnets, 1)
+  vpc_security_group_ids      = [module.security_group_web.security_group_id]
+  associate_public_ip_address = true
+  key_name                    = "ec2-key-pair"
+
+  create_iam_instance_profile = true
+  iam_role_description        = "IAM role for EC2 instance"
+  iam_role_policies = {
+    AdministratorAccess = "arn:aws:iam::aws:policy/AdministratorAccess"
+  }
+
+  user_data_base64            = base64encode(file("./resources/userdata.txt"))
+  user_data_replace_on_change = true
+
+  enable_volume_tags = false
+  root_block_device = [
+    {
+      encrypted   = true
+      volume_type = "gp3"
+      throughput  = 200
+      volume_size = 50
+      tags = {
+        Name = "my-root-block"
+      }
+    },
+  ]
+
+  ebs_block_device = [
+    {
+      device_name = "/dev/sdf"
+      volume_type = "gp3"
+      volume_size = 5
+      throughput  = 200
+      encrypted   = false
+    }
+  ]
+
+    tags = local.tags
+
+}
+
+###########################################################################################
+#####    NETWORK LOAD BALANCER
+###########################################################################################
+
+module "security_group_lb" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "security-group-lb"
+  description = "Security group for lb"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp"]
+  egress_rules        = ["all-all"]
+
+}
+
+resource "aws_lb_target_group_attachment" "test1" {
+  target_group_arn = aws_lb_target_group.test.arn
+  target_id        = module.ec2.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "test2" {
+  target_group_arn = aws_lb_target_group.test.arn
+  target_id        = module.ec2-web2.id
+  port             = 80
+}
+
+resource "aws_lb" "test" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "network"
+  security_groups    = [module.security_group_lb.security_group_id]
+  subnets            = module.vpc.public_subnets
+  enable_cross_zone_load_balancing = true
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+resource "aws_lb_target_group" "test" {
+  name     = "tf-example-lb-tg"
+  port     = 80
+  protocol = "TCP"
+  vpc_id   = module.vpc.vpc_id
+}
+
+resource "aws_lb_listener" "test" {
+  load_balancer_arn = aws_lb.test.arn
+  port              = "80"
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.test.arn
+  }
 }
--- network_course/loadbalancer/variables.tf
+++ network_course/loadbalancer/variables.tf
@@ -7,8 +7,8 @@ variable "network_structure" {
   type = map(any)
   default = {
     cidr    = ["10.0.0.0/16"]
-    azs     = ["eu-central-1a"]
-    public  = ["10.0.1.0/24"]
-    private = ["10.0.11.0/24"]
+    azs     = ["eu-central-1a","eu-central-1b"]
+    public  = ["10.0.1.0/24","10.0.2.0/24"]
+    private = ["10.0.11.0/24","10.0.12.0/24"]
   }
 }
