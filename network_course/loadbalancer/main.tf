028b3d944ea48efd0c1a0106992791ea024ea5f6
andrea <andrea.bortolossi89@gmail.com>
2023-09-28 12:33:23 +0200

    Webserver working

--- network_course/loadbalancer/main.tf
+++ network_course/loadbalancer/main.tf
@@ -28,3 +28,69 @@ module "vpc" {
     Environment = "all"
   }
 }
+
+module "security_group" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "security-group-web-server"
+  description = "Security group for webserver instance"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["ssh-tcp","all-icmp","http-80-tcp"]
+  egress_rules        = ["all-all"]
+
+}
+
+resource "aws_key_pair" "this" {
+  key_name   = "ec2-key-pair"
+  public_key = file("./resources/id_rsa.pub")
+}
+
+module "ec2" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  name                        = "ec2-iac-tool"
+  ami                         = "ami-0a261c0e5f51090b1"
+  instance_type               = "t2.micro"
+  availability_zone           = element(module.vpc.azs, 0)
+  subnet_id                   = element(module.vpc.public_subnets, 0)
+  vpc_security_group_ids      = [module.security_group.security_group_id]
+  associate_public_ip_address = true
+  key_name                    = "ec2-key-pair"
+
+  create_iam_instance_profile = true
+  iam_role_description        = "IAM role for EC2 instance"
+  iam_role_policies = {
+    AdministratorAccess = "arn:aws:iam::aws:policy/AdministratorAccess"
+  }
+
+  user_data_base64            = base64encode(file("./resources/userdata.txt"))
+  user_data_replace_on_change = true
+
+  enable_volume_tags = false
+  root_block_device = [
+    {
+      encrypted   = true
+      volume_type = "gp3"
+      throughput  = 200
+      volume_size = 50
+      tags = {
+        Name = "my-root-block"
+      }
+    },
+  ]
+
+  ebs_block_device = [
+    {
+      device_name = "/dev/sdf"
+      volume_type = "gp3"
+      volume_size = 5
+      throughput  = 200
+      encrypted   = false
+    }
+  ]
+
+    tags = local.tags
+
+}
