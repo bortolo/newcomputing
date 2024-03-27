a87f62be932f705413f9b02997d753d34c306e18
andrea <andrea.bortolossi89@gmail.com>
2024-03-27 09:24:37 +0100

    role from file

--- developer_course/Lambda/main.tf
+++ developer_course/Lambda/main.tf
@@ -5,27 +5,12 @@ locals {
   }
 }
 resource "aws_iam_role" "lambda_role" {
-name   = "Spacelift_Test_Lambda_Function_Role"
-assume_role_policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-   {
-     "Action": "sts:AssumeRole",
-     "Principal": {
-       "Service": "lambda.amazonaws.com"
-     },
-     "Effect": "Allow",
-     "Sid": ""
-   }
- ]
-}
-EOF
+name                = "MyTest_Lambda_Function_Role"
+assume_role_policy  = file("./resources/lambda_role.json")
 tags = local.tags
 }
 
 resource "aws_iam_policy" "iam_policy_for_lambda" {
- 
   name         = "aws_iam_policy_for_terraform_aws_lambda_role"
   path         = "/"
   description  = "AWS IAM Policy for managing aws lambda role"
