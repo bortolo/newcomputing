a7cb162ad065e5e46d52bec032217b79a8dcbbd7
andrea <andrea.bortolossi89@gmail.com>
2024-03-27 09:19:34 +0100

    policy from file

--- developer_course/Lambda/main.tf
+++ developer_course/Lambda/main.tf
@@ -26,26 +26,12 @@ tags = local.tags
 
 resource "aws_iam_policy" "iam_policy_for_lambda" {
  
- name         = "aws_iam_policy_for_terraform_aws_lambda_role"
- path         = "/"
- description  = "AWS IAM Policy for managing aws lambda role"
- policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-   {
-     "Action": [
-       "logs:CreateLogGroup",
-       "logs:CreateLogStream",
-       "logs:PutLogEvents"
-     ],
-     "Resource": "arn:aws:logs:*:*:*",
-     "Effect": "Allow"
-   }
- ]
-}
-EOF
-tags = local.tags
+  name         = "aws_iam_policy_for_terraform_aws_lambda_role"
+  path         = "/"
+  description  = "AWS IAM Policy for managing aws lambda role"
+  policy       = file("./resources/lambda_policy.json")
+  tags = local.tags
+
 }
 
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
