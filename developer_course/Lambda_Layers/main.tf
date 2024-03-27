45ad393ab4b41657d57cd26aa690e4a10ee4b762
andrea <andrea.bortolossi89@gmail.com>
2024-03-27 17:39:43 +0100

    lambda layers

---
+++ developer_course/Lambda_Layers/main.tf
@@ -0,0 +1,40 @@
+locals {
+  tags = {
+    Owner       = "andrea.bortolossi"
+    Name        = "developercourse-lambda-layers"
+  }
+}
+resource "aws_iam_role" "lambda_role" {
+name                = "MyTest_Lambda_Function_Role"
+assume_role_policy  = file("./resources/lambda_role.json")
+tags = local.tags
+}
+
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+  name         = "aws_iam_policy_for_terraform_aws_lambda_role"
+  path         = "/"
+  description  = "AWS IAM Policy for managing aws lambda role"
+  policy       = file("./resources/lambda_policy.json")
+  tags = local.tags
+
+}
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+ role        = aws_iam_role.lambda_role.name
+ policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
+}
+
+
+resource "aws_lambda_function" "terraform_lambda_func" {
+filename                       = "./resources/hello-python-v${var.code_version}.zip"
+function_name                  = "Lambda_${local.tags.Name}_v${var.code_version}"
+role                           = aws_iam_role.lambda_role.arn
+handler                        = "index${var.code_version}.lambda_handler"
+runtime                        = "python3.8"
+depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+
+# to get the right arn you can manually deploy the lambda fucntion and then pick it up from the lambda console
+layers           = ["arn:aws:lambda:eu-central-1:292169987271:layer:AWSLambda-Python38-SciPy1x:107"]
+
+tags = local.tags
+}
