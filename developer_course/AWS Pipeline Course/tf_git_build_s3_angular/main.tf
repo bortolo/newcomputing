5da162552c6eada0793aaf1357ffa82f5ce7a439
andrea <andrea.bortolossi89@gmail.com>
2024-10-13 19:25:41 +0200

    pipeline angular working

---
+++ developer_course/AWS Pipeline Course/tf_git_build_s3_angular/main.tf
@@ -0,0 +1,308 @@
+locals {
+  tags = {
+    Owner       = "andrea.bortolossi"
+    Name        = "AWS-course-codepipeline"
+  }
+}
+
+#######################################################################################
+# PROD BUCKET website
+#######################################################################################
+
+# Bucket S3 per il website
+resource "aws_s3_bucket" "prod_bucket" {
+  bucket = var.bucket_prod
+  tags = local.tags
+}
+
+resource "aws_s3_bucket_website_configuration" "prod_bucket" {
+  bucket = aws_s3_bucket.prod_bucket.id
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "prod_bucket" {
+  bucket = aws_s3_bucket.prod_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "prod_bucket" {
+  bucket = aws_s3_bucket.prod_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Configura le policy del bucket per permettere l'accesso pubblico
+resource "aws_s3_bucket_policy" "allow_public_access" {
+  bucket = aws_s3_bucket.prod_bucket.id
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": "s3:GetObject",
+        "Resource": "${aws_s3_bucket.prod_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+#######################################################################################
+# CODEPIPELINE 
+#######################################################################################
+
+# Bucket S3 per artifact store
+resource "aws_s3_bucket" "artifact_bucket" {
+  bucket = "${var.pipeline_name}-artifact-store"
+  tags = local.tags
+}
+
+resource "aws_s3_bucket_versioning" "artifact_bucket" {
+  bucket = aws_s3_bucket.artifact_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Crea il ruolo IAM per CodePipeline
+resource "aws_iam_role" "codepipeline_role" {
+  name = "${var.pipeline_name}_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = {
+        Service = "codepipeline.amazonaws.com"
+      }
+    }]
+  })
+
+    tags = local.tags
+}
+
+# Attacca policy al ruolo di CodePipeline
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  role = aws_iam_role.codepipeline_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ],
+        Resource = [
+          "*"
+        ]
+      },
+      {
+        Effect   = "Allow",
+        Action   = [
+          "codestar-connections:*"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow",
+        Action   = [
+          "codebuild:StartBuild",
+          "codebuild:BatchGetBuilds"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Connessione a github
+# La connessione deve essere già disponibile per questa region
+data "aws_codestarconnections_connection" "example" {
+  name = var.github_connection_name
+}
+
+# Definisci il CodePipeline
+resource "aws_codepipeline" "my_pipeline" {
+  name     = var.pipeline_name
+  pipeline_type = "V2"
+  execution_mode = "QUEUED"
+  role_arn = aws_iam_role.codepipeline_role.arn
+  
+
+  artifact_store {
+    location = aws_s3_bucket.artifact_bucket.bucket
+    type     = "S3"
+  }
+
+
+  # Fase di origine (Source) da GitHub
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        ConnectionArn        = data.aws_codestarconnections_connection.example.arn
+        FullRepositoryId     = var.github_repo
+        BranchName           = "master"
+        DetectChanges        = true
+        OutputArtifactFormat = "CODE_ZIP"
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      version          = "1"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+
+      configuration = {
+        ProjectName = aws_codebuild_project.my_codebuild.name
+      }
+    }
+  }
+
+  # Fase di deploy
+  stage {
+    name = "Deploy"
+
+    action {
+      name             = "DeployAction"
+      category         = "Deploy"
+      owner            = "AWS"
+      provider         = "S3"
+      version          = "1"
+      input_artifacts  = ["build_output"]
+
+      configuration = {
+        BucketName = aws_s3_bucket.prod_bucket.bucket
+        Extract    = "true"
+      }
+    }
+  }
+
+  tags = local.tags
+
+}
+
+#######################################################################################
+# CODEBUILD
+#######################################################################################
+
+# Crea il ruolo IAM per codebuild
+resource "aws_iam_role" "codebuild_role" {
+  name = "${var.codebuild_name}-exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = {
+        Service = "codebuild.amazonaws.com"
+      }
+    }]
+  })
+    tags = local.tags
+}
+
+# Attacca policy al ruolo di codebuild
+resource "aws_iam_role_policy" "codebuild_policy" {
+  role = aws_iam_role.codebuild_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:GetObjectVersion",
+          "s3:GetBucketAcl",
+          "s3:GetBucketLocation",
+          "s3:ListBucket"
+        ],
+        Resource = [
+          "*"
+        ]
+      },
+      {
+        Effect   = "Allow",
+        Action   = [
+          "codebuild:CreateReportGroup",
+          "codebuild:CreateReport",
+          "codebuild:UpdateReport",
+          "codebuild:BatchPutTestCases",
+          "codebuild:BatchPutCodeCoverages"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow",
+        Action   = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+}
+
+# Progetto CodeBuild
+resource "aws_codebuild_project" "my_codebuild" {
+  name                   = "${var.codebuild_name}-update-code"
+  service_role           = aws_iam_role.codebuild_role.arn
+  #concurrent_build_limit = 1
+
+  source {
+    type      = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:7.0" #check you runtime at https://docs.aws.amazon.com/it_it/codebuild/latest/userguide/available-runtimes.html
+    type                        = "LINUX_CONTAINER"
+  }
+
+  # Non si riesce a capire come creare artefatti leggibili su S3
+  # L'alternativa è copiarli con comandi da terminali dentro la build e gestire separatamente la gestione degli artifacts
+  artifacts {
+    type = "CODEPIPELINE"
+    packaging = "NONE"
+    encryption_disabled = true
+    namespace_type = "BUILD_ID"
+  }
+
+  build_timeout = 5
+  tags = local.tags
+}
---
+++ developer_course/AWS Pipeline Course/tf_git_build_s3_angular/terraform.tfvars
@@ -0,0 +1,6 @@
+aws_region               = "eu-central-1"
+bucket_prod              = "prod-mywebsite-bucket"
+github_connection_name   = "my-github-connection"
+github_repo              = "bortolo/my-angular-repo"
+pipeline_name            = "my-github-pipeline"
+codebuild_name           = "my-angular-build"
